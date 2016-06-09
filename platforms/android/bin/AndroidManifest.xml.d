e0dcd0b7ba7d63c17de506f2fb39a1b67580185c
minhhieuha <minhhieu_106@yahoo.com>
2016-06-09 22:37:44 +0700

    ok
    ok

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\bin\AndroidManifest.xml : \
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\AndroidManifest.xml \
--- platforms/android/bin/ComeraTest-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\bin\ComeraTest-debug-unaligned.apk : \
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\bin\ComeraTest.ap_ \
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\bin\classes.dex \
-E:\PD\Workspaces\Cordova\CameraTest\platforms\android\libs\cordova-3.0.0.jar \
